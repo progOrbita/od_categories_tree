b51718bcd716ca8cf03fdba65e476ab05b322920
progOrbita <web.orbitadigital@gmail.com>
2023-03-10 12:05:26 +0100

    Update script.js
    add event listener on click to expand or collapse the tree

--- views/js/script.js
+++ views/js/script.js
@@ -1,1 +1,12 @@
+$(document).on('click', '.expand', function () {
+    $('.expand i').removeClass('fa-angle-up');
+    if ($('ul:first').hasClass('colapsed')) {
+        $('ul:first').removeClass('colapsed');
+        $('.expand i').removeClass('fa-angle-down');
+        $('.expand i').addClass('fa-angle-up');
+        return;
+    }
 
+    $('.expand i').addClass('fa-angle-down');
+    $('ul:first').addClass('colapsed');
+});
