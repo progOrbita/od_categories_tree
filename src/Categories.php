caee1c409665d47ebd46f47d38cc726710cd6107
progOrbita <web.orbitadigital@gmail.com>
2023-03-10 11:56:29 +0100

    Update Categories.php
    add getCategories() function to get all categories saved on 2 arrays

--- src/Categories.php
+++ src/Categories.php
@@ -23,4 +23,20 @@ class Categories
         $this->id_root = (int) Db::getInstance()->getValue("SELECT id_category FROM ps_category WHERE is_root_category=1");
     }
 
+    /**
+     * get all categories of our db and save the info on two arrays
+     */
+
+    private function getCategories()
+    {
+        $exe = Db::getInstance()->executeS("SELECT ps_category.id_category, ps_category.id_parent, ps_category_lang.name 
+        FROM ps_category 
+        LEFT JOIN ps_category_lang ON ps_category.id_category=ps_category_lang.id_category 
+        WHERE ps_category_lang.id_lang=" . $this->id_lang);
+        foreach ($exe as $key) {
+            $this->categories[$key["id_category"]] = ["name" => $key["name"], "id_parent" => $key["id_parent"]];
+            $this->parentChildren[$this->categories[$key["id_category"]]['id_parent']][] = $key["id_category"];
+        }
+    }
+
 }
