809a6bbda231743210a49e3b0e642a3d1840926c
progOrbita <web.orbitadigital@gmail.com>
2023-03-10 12:00:14 +0100

    Update Tree.php
    add makeTree() to make a html tree

--- src/Tree.php
+++ src/Tree.php
@@ -2,4 +2,38 @@
 
 class Tree
 {
+
+    /**
+     * function to make html tree 
+     * 
+     * @param array $data
+     * @param string $txt
+     * 
+     * @return string
+     */
+
+    public static function makeTree(array $data, string $txt = ''): string
+    {
+        if (empty($data)) {
+            return '';
+        }
+
+        $txt .= '<ul class="collapse show">';
+
+        foreach ($data as $key => $cat) {
+            if (isset($cat['children'])) {
+                $txt .= ' <li class=" less" id=' . $key . ' "><div class="checkbox"><i class="fa fa-angle-down" onclick="openClose(' . $key . ')"></i><label><input class="input" name="' . $cat['name'] . '" value="' . $key . '"type="checkbox">' . $cat['name'] . ' <input class="radio" type="radio"></div></label>';
+                if (isset($cat['children'])) {
+                    $txt .= self::makeTree($cat['children']);
+                }
+                continue;
+            }
+
+            $txt .= ' <li class=""><div class="checkbox"><label><input class="input" name="' . $cat['name'] . '" value="' . $key . '"type="checkbox">' . $cat['name'] . ' <input class="radio" type="radio"></label></div></li>';
+        }
+
+        $txt .= '</li></ul>';
+
+        return $txt;
+    }
 }
